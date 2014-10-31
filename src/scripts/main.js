42e4b8118763d7fb9d43c1401bd87892ef36935b
Nick Peters <nkpeters@amazon.com>
2014-10-30 20:10:27 -0700

    Added controls and CSS.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,48 @@
 require("script!jquery/dist/jquery.js");
 require("script!jquery-touchswipe/jquery.touchSwipe.js");
 
+$.fn.swipe.defaults.excludedElements = "input, select, textarea, a, .noSwipe";
+
 $('ul#items').swipe({
   swipeLeft: function(event, direction, distance, duration, fingerCount, fingerData) {
-    var currentEl = $('.active');
-    var nextEl = currentEl.next();
-    if (nextEl.length > 0) {
-      currentEl.attr('class', 'item');
-      nextEl.attr('class', 'item active');
-    }
+    next();
   },
   swipeRight: function(event, direction, distance, duration, fingerCount, fingerData) {
-    var currentEl = $('.active');
-    var previousEl = currentEl.prev();
-    if (previousEl.length > 0) {
-      currentEl.attr('class', 'item');
-      previousEl.attr('class', 'item active');
-    }
+    previous();
   },
   tap: function(event, target) {
-    var altText = $(target).attr('alt');
-    console.log("Tap Event");
+    var aboutText = $(target).data('about');
+    $('#about').html(aboutText);
   },
   threshold: 25
 });
+
+$('button#next').swipe({
+  tap: function(event, target) {
+    next();
+  }
+});
+
+$('button#previous').swipe({
+  tap: function(event, target) {
+    previous();
+  }
+});
+
+var previous = function() {
+  var currentEl = $('.active');
+  var previousEl = currentEl.prev();
+  if (previousEl.length > 0) {
+    currentEl.attr('class', 'item');
+    previousEl.attr('class', 'item active');
+  }
+};
+
+var next = function() {
+  var currentEl = $('.active');
+  var nextEl = currentEl.next();
+  if (nextEl.length > 0) {
+    currentEl.attr('class', 'item');
+    nextEl.attr('class', 'item active');
+  }
+};
